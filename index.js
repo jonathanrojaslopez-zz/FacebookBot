b8e5dca4d397c23d7940f2d315ad090cfd8b783f
jonathanrojas <jonathanrojaslopez@gmail.com>
2017-05-02 17:45:14 -0500

    heroku test

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ app.listen(app.get('port'), function() {
 });
 
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'johnny_token') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook',function (req,res) {
+	res.sendStatus(200)
+})
